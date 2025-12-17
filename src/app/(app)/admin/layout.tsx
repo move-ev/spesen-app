c2d35276cced8546e27f59026f0f3dbd4408fe1c
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-12-17 18:49:57 +0100

    refactor: reorganize app folder structure

---
+++ src/app/(app)/admin/layout.tsx
@@ -0,0 +1,30 @@
+import { APP_ROUTES } from "@/lib/routes";
+import { auth } from "@/server/better-auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+/**
+ * Checks if the user is logged in an if they are allowed to access
+ * the admin area
+ */
+export default async function ServerLayout({
+  children,
+}: React.PropsWithChildren) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  // The main app layout will check if the user is logged in
+  // and redirect to the login page if not. We can safely assume that
+  // the user is logged in at this point.
+
+  //   if (!session?.user) {
+  //     redirect(AUTH_ROUTES.LOGIN);
+  //   }
+
+  if (session?.user?.role !== "admin") {
+    redirect(APP_ROUTES.HOME);
+  }
+
+  return children;
+}
