bfdf38dfa7a9908ddca1490c030493c3e34b07a1
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-12-17 17:37:25 +0100

    feat: add create sheet to business units

--- src/app/admin/settings/business-unit/page.tsx
+++ src/app/admin/settings/business-unit/page.tsx
@@ -1,14 +1,44 @@
+import { CreateBusinessUnitForm } from "@/components/forms/create-business-unit";
 import { PageTitle } from "@/components/page-title";
 import { BusinessUnitsTable } from "@/components/tables/business-units";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { api } from "@/trpc/server";
+import { PlusIcon } from "lucide-react";
 
 export default async function ServerPage() {
   void api.businessUnit.list.prefetch();
 
   return (
     <div>
-      <header>
+      <header className="flex flex-wrap items-center justify-between gap-4">
         <PageTitle>Geschäftseinheiten</PageTitle>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"outline"}>
+              <PlusIcon />
+              Geschäftseinheit erstellen
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Geschäftseinheit erstellen</SheetTitle>
+              <SheetDescription>
+                Erstelle eine neue Geschäftseinheit
+              </SheetDescription>
+            </SheetHeader>
+            <div className="p-4">
+              <CreateBusinessUnitForm />
+            </div>
+          </SheetContent>
+        </Sheet>
       </header>
       <div className="mt-12">
         <BusinessUnitsTable />
