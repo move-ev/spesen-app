ac4e68e2f84f0d0d9e572889fba9d8b2fa1f3bf7
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-12-17 17:01:08 +0100

    refactor: remove boilerplate code

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,12 +1,16 @@
 import { env } from "@/env";
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
 
-const createPrismaClient = () =>
-  new PrismaClient({
+const createPrismaClient = () => {
+  const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
+  return new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
-
+};
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
