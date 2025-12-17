d333f2bb3cb94d6a7c99d18fc62f2fb2af1b0b19
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-12-17 19:15:44 +0100

    feat(ui): implement user menu

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -4,4 +4,7 @@ export const authRouter = createTRPCRouter({
   isAdmin: protectedProcedure.query(async ({ ctx }) => {
     return ctx.session?.user?.role === "admin";
   }),
+  getCurrentSession: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.session;
+  }),
 });
