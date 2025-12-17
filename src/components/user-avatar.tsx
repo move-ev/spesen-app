bb8403afdee95af08e4f7a73a34313e5406863cb
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-12-17 19:10:48 +0100

    feat(ui): implement user avatar

---
+++ src/components/user-avatar.tsx
@@ -0,0 +1,37 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import type { User } from "better-auth";
+import React from "react";
+
+export function UserAvatar({
+  user,
+  ...props
+}: React.ComponentProps<typeof Avatar> & {
+  user: User;
+}) {
+  const initials = React.useMemo(() => {
+    if (!user.name) return "?";
+
+    const [firstName, lastName] = user.name.split(" ");
+
+    if (!firstName && !lastName) return "?";
+
+    let initials = "";
+
+    if (firstName) {
+      initials += firstName.charAt(0).toUpperCase();
+    }
+
+    if (lastName) {
+      initials += lastName.charAt(0).toUpperCase();
+    }
+
+    return initials;
+  }, [user.name]);
+
+  return (
+    <Avatar {...props}>
+      <AvatarImage src={user.image ?? ""} />
+      <AvatarFallback>{initials}</AvatarFallback>
+    </Avatar>
+  );
+}
